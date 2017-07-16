a38c84846a7c99beba5f9547c1df304990c69c04
youta1119 <youta1119ume@gmail.com>
2017-07-17 00:12:55 +0900

    Fix module

--- src/main/kotlin/com/github/youta1119/either_kotlin/Either.kt
+++ src/main/kotlin/com/github/youta1119/either_kotlin/Either.kt
@@ -1,8 +1,22 @@
 package com.github.youta1119.either_kotlin
 
-/**
- * Created by youta on 2017/07/12.
- */
+@Suppress("unused")
+sealed class Either<A, B> {
+    abstract val isLeft: Boolean
+    abstract val isRight: Boolean
+}
+
+@Suppress("unused")
+data class Left<A, B>(val value: A) : Either<A, B>() {
+    override val isLeft: Boolean = true
+    override val isRight: Boolean = false
+}
+
+@Suppress("unused")
+data class Right<A, B>(val value: B) : Either<A, B>() {
+    override val isLeft: Boolean = false
+    override val isRight: Boolean = true
+}
 
 @Suppress("unused")
 inline fun <A, B, X> Either<A, B>.fold(left: (A) -> X, right: (B) -> X): X = when (this) {
@@ -16,6 +30,18 @@ fun <A, B> Either<A, B>.swap(): Either<B, A> = when (this) {
     is Left -> Right(value)
 }
 
+@Suppress("UNCHECKED_CAST", "unused")
+fun <A, B : Either<A1, C>, A1, C> Either<A, B>.joinRight(): Either<A1, C> = when (this) {
+    is Right -> this.value
+    is Left -> this as Either<A1, C>
+}
+
+@Suppress("UNCHECKED_CAST", "unused")
+fun <A : Either<C, B1>, B, B1, C> Either<A, B>.joinLeft(): Either<C, B1> = when (this) {
+    is Right -> this as Either<C, B1>
+    is Left -> this.value
+}
+
 @Suppress("unused")
 inline fun <A, B> Either<A, B>.foreach(action: (B) -> Unit): Unit = when (this) {
     is Right -> action(this.value)
@@ -23,13 +49,13 @@ inline fun <A, B> Either<A, B>.foreach(action: (B) -> Unit): Unit = when (this) 
 }
 
 @Suppress("unused")
-fun <A, B> Either<A, B>.getOrElse(defaultValue: B): B = when (this) {
+fun <A, B : B1, B1> Either<A, B>.getOrElse(defaultValue: B1): B1 = when (this) {
     is Right -> this.value
     is Left -> defaultValue
 }
 
 @Suppress("unused")
-fun <A, B> Either<A, B>.contains(elem: B): Boolean = when (this) {
+fun <A, B : B1, B1> Either<A, B>.contains(elem: B1): Boolean = when (this) {
     is Right -> elem == this.value
     is Left -> false
 }
@@ -61,9 +87,9 @@ inline fun <A, B, C> Either<A, B>.map(transform: (B) -> C): Either<A, C> = when 
 
 
 @Suppress("UNCHECKED_CAST", "unused")
-inline fun <A, B> Either<A, B>.filterOrElse(predicate: (B) -> Boolean,defaultValue: A): Either<A, B> = when (this) {
-    is Right -> if (predicate(this.value)) this else Left(defaultValue)
-    is Left -> this
+inline fun <A : A1, B, A1> Either<A, B>.filterOrElse(predicate: (B) -> Boolean, defaultValue: A1): Either<A1, B> = when (this) {
+    is Right -> if (predicate(this.value)) this as Either<A1, B> else Left(defaultValue)
+    is Left -> this as Either<A1, B>
 }
 
 
@@ -78,9 +104,6 @@ fun <A, B> Either<A, B>.toList(): List<B> = when (this) {
     is Right -> listOf(this.value)
     is Left -> emptyList()
 }
-
-
-
 
 
 
